9afde74343436635d089ced018c35dc59922c36f
Adam Fowler <adamfowler71@gmail.com>
2021-04-15 15:11:56 +0100

    Use hummingbird-mustache to render source files

--- Package.swift
+++ Package.swift
@@ -11,6 +11,7 @@ let package = Package(
         .package(url: "https://github.com/adam-fowler/soto-smithy.git", .branch("main")),
         .package(url: "https://github.com/apple/swift-argument-parser.git", .upToNextMajor(from: "0.3.0")),
         .package(url: "https://github.com/soto-project/Stencil.git", .upToNextMajor(from: "0.13.2")),
+        .package(url: "https://github.com/hummingbird-project/hummingbird-mustache.git", from: "0.5.2"),
         .package(url: "https://github.com/nicklockwood/SwiftFormat.git", .upToNextMinor(from: "0.47.4")),
     ],
     targets: [
@@ -20,6 +21,7 @@ let package = Package(
                 .product(name: "SotoSmithy", package: "soto-smithy"),
                 .product(name: "SotoSmithyAWS", package: "soto-smithy"),
                 .product(name: "ArgumentParser", package: "swift-argument-parser"),
+                .product(name: "HummingbirdMustache", package: "hummingbird-mustache"),
                 .product(name: "Stencil", package: "Stencil"),
                 .product(name: "SwiftFormat", package: "SwiftFormat")
             ],
