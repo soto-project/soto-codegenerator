39759d3e6c78a8d96508b2c6b4ca67f1af916e6e
Adam Fowler <adamfowler71@gmail.com>
2021-10-20 19:43:39 +0100

    Only query protocols, should check query error trait (#31)

--- Sources/SotoCodeGenerator/Templates/paginator+async.swift
+++ Sources/SotoCodeGenerator/Templates/paginator+async.swift
@@ -23,7 +23,7 @@ extension Templates {
 
     // MARK: Paginators
 
-    @available(macOS 9999, iOS 9999, watchOS 9999, tvOS 9999, *)
+    @available(macOS 12.0, iOS 15.0, watchOS 8.0, tvOS 15.0, *)
     extension {{name}} {
     {{#paginators}}
     {{#operation.comment}}
