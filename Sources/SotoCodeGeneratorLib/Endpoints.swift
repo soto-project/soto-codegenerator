769ef9e36f7a705be8160187c94dd924222843b2
Adam Fowler <adamfowler71@gmail.com>
2022-11-02 17:00:21 +0000

    Remove region enum, replace with String

--- Sources/SotoCodeGeneratorLib/Endpoints.swift
+++ Sources/SotoCodeGeneratorLib/Endpoints.swift
@@ -2,7 +2,7 @@
 //
 // This source file is part of the Soto for AWS open source project
 //
-// Copyright (c) 2017-2020 the Soto project authors
+// Copyright (c) 2017-2022 the Soto project authors
 // Licensed under Apache License v2.0
 //
 // See LICENSE.txt for license information
@@ -11,44 +11,6 @@
 // SPDX-License-Identifier: Apache-2.0
 //
 //===----------------------------------------------------------------------===//
-
-enum Region: String, Decodable {
-    case useast1 = "us-east-1"
-    case useast2 = "us-east-2"
-    case uswest1 = "us-west-1"
-    case uswest2 = "us-west-2"
-    case apsouth1 = "ap-south-1"
-    case apsouth2 = "ap-south-2"
-    case apsoutheast1 = "ap-southeast-1"
-    case apnortheast2 = "ap-northeast-2"
-    case apnortheast3 = "ap-northeast-3"
-    case apnortheast1 = "ap-northeast-1"
-    case apsoutheast2 = "ap-southeast-2"
-    case apsoutheast3 = "ap-southeast-3"
-    case apeast1 = "ap-east-1"
-    case cacentral1 = "ca-central-1"
-    case euwest1 = "eu-west-1"
-    case euwest3 = "eu-west-3"
-    case euwest2 = "eu-west-2"
-    case eucentral1 = "eu-central-1"
-    case eucentral2 = "eu-central-2"
-    case eunorth1 = "eu-north-1"
-    case eusouth1 = "eu-south-1"
-    case eusouth2 = "eu-south-2"
-    case saeast1 = "sa-east-1"
-    case mecentral1 = "me-central-1"
-    case mesouth1 = "me-south-1"
-    case afsouth1 = "af-south-1"
-
-    case cnnorth1 = "cn-north-1"
-    case cnnorthwest1 = "cn-northwest-1"
-
-    case usgoveast1 = "us-gov-east-1"
-    case usgovwest1 = "us-gov-west-1"
-    case usisoeast1 = "us-iso-east-1"
-    case usisobeast1 = "us-isob-east-1"
-    case usisowest1 = "us-iso-west-1"
-}
 
 enum SignatureVersion: String, Decodable {
     case v2
@@ -59,7 +21,7 @@ enum SignatureVersion: String, Decodable {
 
 struct Endpoints: Decodable {
     struct CredentialScope: Decodable {
-        var region: Region?
+        var region: String?
         var service: String?
     }
 
