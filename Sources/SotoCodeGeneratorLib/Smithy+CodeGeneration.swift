17bf0fe4f5a8c817c64d849adf88c32794f5cffd
Adam Fowler <adamfowler71@gmail.com>
2023-07-10 12:26:52 +0100

    Rename HTTPBody to AWSHTTPBody

--- Sources/SotoCodeGeneratorLib/Smithy+CodeGeneration.swift
+++ Sources/SotoCodeGeneratorLib/Smithy+CodeGeneration.swift
@@ -99,7 +99,7 @@ extension MemberShape {
             if memberShape.hasTrait(type: EnumTrait.self) { return self.target.shapeName.toSwiftClassCase() }
             return "String"
         } else if memberShape is BlobShape {
-            if self.hasTrait(type: HttpPayloadTrait.self) { return "HTTPBody" }
+            if self.hasTrait(type: HttpPayloadTrait.self) { return "AWSHTTPBody" }
             return "AWSBase64Data"
         } else if memberShape is CollectionShape {
             return self.target.shapeName.toSwiftClassCase()
@@ -123,7 +123,7 @@ extension MemberShape {
             if memberShape.hasTrait(type: EnumTrait.self) { return "\(withServiceName).\(self.target.shapeName.toSwiftClassCase())" }
             return "String"
         } else if memberShape is BlobShape {
-            if self.hasTrait(type: HttpPayloadTrait.self) { return "HTTPBody" }
+            if self.hasTrait(type: HttpPayloadTrait.self) { return "AWSHTTPBody" }
             return "AWSBase64Data"
         } else if memberShape is CollectionShape {
             return "\(withServiceName).\(self.target.shapeName.toSwiftClassCase())"
