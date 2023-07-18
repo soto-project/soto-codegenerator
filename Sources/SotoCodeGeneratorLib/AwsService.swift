3f98adae6e3c9ba39fff463ac0988c0b8a17c1b0
Adam Fowler <adamfowler71@gmail.com>
2023-07-18 08:10:56 +0100

    Generate response header decoding functions (#73)
    * Rename HTTPBody to AWSHTTPBody
    
    * Get headers, payload from Codable userinfo
    
    Initial commit
    
    * Add output for different decoding setups
    
    * fix enum with assoiciated values
    
    * Fix paginators, ResponseDecodingContainer
    
    * Decode propertyWrapper values correctly
    
    * Don't add AWSShapeWithPayload conformance for output shapes
    
    * Only output init(from:) is required
    
    * Remove commented out code

--- Sources/SotoCodeGeneratorLib/AwsService.swift
+++ Sources/SotoCodeGeneratorLib/AwsService.swift
@@ -48,6 +48,8 @@ struct AwsService {
         self.endpoints = endpoints
         self.outputHTMLComments = outputHTMLComments
         self.logger = logger
+
+        self.markInputOutputShapes(model)
     }
 
     /// Return service name from API
@@ -467,12 +469,12 @@ struct AwsService {
             }
         }
         // if output token is member of an optional struct add ? suffix
-
-        if let member = structure.members?[String(split[0])],
-           !member.hasTrait(type: RequiredTrait.self),
-           split.count > 1
-        {
-            split[0] += "?"
+        if let member = structure.members?[String(split[0])] {
+            let required = member.hasTrait(type: RequiredTrait.self) ||
+                (member.hasTrait(type: HttpPayloadTrait.self) && structure.hasTrait(type: SotoOutputShapeTrait.self))
+            if !required, split.count > 1 {
+                split[0] += "?"
+            }
         }
         return split.map { String($0).toSwiftVariableCase() }.joined(separator: ".")
     }
@@ -598,21 +600,21 @@ struct AwsService {
             if usedInOutput {
                 shapeProtocol += " & AWSDecodableShape"
             }
+            if hasPayload {
+                shapeProtocol += " & AWSShapeWithPayload"
+            }
         } else if usedInOutput {
             shapeProtocol = "AWSDecodableShape"
         } else {
             return nil
         }
-        if hasPayload {
-            shapeProtocol += " & AWSShapeWithPayload"
-        }
         return shapeProtocol
     }
 
-    func isMemberInBody(_ member: MemberShape) -> Bool {
+    func isMemberInBody(_ member: MemberShape, isOutputShape: Bool) -> Bool {
         return !(member.hasTrait(type: HttpHeaderTrait.self) ||
             member.hasTrait(type: HttpPrefixHeadersTrait.self) ||
-            member.hasTrait(type: HttpQueryTrait.self) ||
+            (member.hasTrait(type: HttpQueryTrait.self) && !isOutputShape) ||
             member.hasTrait(type: HttpLabelTrait.self) ||
             member.hasTrait(type: HttpResponseCodeTrait.self))
     }
@@ -689,6 +691,15 @@ extension AwsService {
         let value: String
     }
 
+    struct MemberDecodeContext {
+        var fromHeader: String?
+        var fromPayload: Bool?
+        var fromRawPayload: Bool?
+        var fromCodable: Bool?
+        var fromStatusCode: Bool?
+        var decodeType: String
+    }
+
     struct MemberContext {
         let variable: String
         let parameter: String
@@ -699,6 +710,7 @@ extension AwsService {
         let comment: [String.SubSequence]
         let deprecated: Bool
         var duplicate: Bool
+        var decoding: MemberDecodeContext
     }
 
     struct InitParamContext {
@@ -763,6 +775,11 @@ extension AwsService {
         var endpoints: [(region: String, hostname: String)] = []
     }
 
+    struct DecodeContext {
+        let requiresResponse: Bool
+        let requiresDecodeInit: Bool
+    }
+
     struct StructureContext {
         let object: String
         let name: String
@@ -771,7 +788,7 @@ extension AwsService {
         var options: String?
         let namespace: String?
         let isEncodable: Bool
-        let isDecodable: Bool
+        let decode: DecodeContext?
         let encoding: [EncodingPropertiesContext]
         let members: [MemberContext]
         let initParameters: [InitParamContext]
