6fc6553cddd35fcf1c6fa37b2e7c0cee487ba71f
Adam Fowler <adamfowler71@gmail.com>
2023-08-02 08:59:12 +0100

    Extra init for services which already have middleware
    Separate out endpoint lists so we don't need to repeat them in both inits

--- Sources/SotoCodeGeneratorLib/Templates/api.swift
+++ Sources/SotoCodeGeneratorLib/Templates/api.swift
@@ -52,7 +52,12 @@ extension Templates {
     {{/regionalized}}
         ///     - partition: AWS partition where service resides, standard (.aws), china (.awscn), government (.awsusgov).
         ///     - endpoint: Custom endpoint URL to use instead of standard AWS servers
+    {{^middlewareClass}}
+        ///     - middleware: Middleware chain used to edit requests before they sent and responses before they are decoded 
+    {{/middlewareClass}}
         ///     - timeout: Timeout value for HTTP requests
+        ///     - byteBufferAllocator: Allocator for ByteBuffers
+        ///     - options: Service options
         {{scope}} init(
             client: AWSClient,
     {{#regionalized}}
@@ -60,6 +65,9 @@ extension Templates {
     {{/regionalized}}
             partition: AWSPartition = .aws,
             endpoint: String? = nil,
+    {{^middlewareClass}}
+            middleware: AWSMiddlewareProtocol?,
+    {{/middlewareClass}}
             timeout: TimeAmount? = nil,
             byteBufferAllocator: ByteBufferAllocator = ByteBufferAllocator(),
             options: AWSServiceConfig.Options = []
@@ -85,29 +93,13 @@ extension Templates {
                 apiVersion: "{{apiVersion}}",
                 endpoint: endpoint,
     {{#first(serviceEndpoints)}}
-                serviceEndpoints: [
-    {{#serviceEndpoints}}
-                    {{.}}{{^last()}},{{/last()}}
-    {{/serviceEndpoints}}
-                ],
+                serviceEndpoints: Self.serviceEndpoints,
     {{/first(serviceEndpoints)}}
     {{#first(partitionEndpoints)}}
-                partitionEndpoints: [
-    {{#partitionEndpoints}}
-                    {{.}}{{^last()}},{{/last()}}
-    {{/partitionEndpoints}}
-                ],
+                partitionEndpoints: Self.partitionEndpoints,
     {{/first(partitionEndpoints)}}
     {{#first(variantEndpoints)}}
-                variantEndpoints: [
-    {{#variantEndpoints}}
-                    [{{variant}}]: .init(endpoints: [
-    {{#endpoints.endpoints}}
-                        "{{region}}": "{{hostname}}"{{^last()}},{{/last()}}
-    {{/endpoints.endpoints}}
-                    ]){{^last()}},{{/last()}}
-    {{/variantEndpoints}}
-                ],
+                variantEndpoints: Self.variantEndpoints,
     {{/first(variantEndpoints)}}
     {{#errorTypes}}
                 errorType: {{.}}.self,
@@ -115,8 +107,8 @@ extension Templates {
     {{#xmlNamespace}}
                 xmlNamespace: "{{.}}",
     {{/xmlNamespace}}
-    {{#middlewareClass}}
-                middleware: {{.}},
+    {{^middlewareClass}}
+                middleware: middleware,
     {{/middlewareClass}}
                 timeout: timeout,
                 byteBufferAllocator: byteBufferAllocator,
@@ -126,6 +118,111 @@ extension Templates {
             self.endpointStorage = .init()
             {{/endpointDiscovery}}
         }
+
+    {{#middlewareClass}}
+        /// Initialize the {{name}} client
+        /// - parameters:
+        ///     - client: AWSClient used to process requests
+    {{#regionalized}}
+        ///     - region: Region of server you want to communicate with. This will override the partition parameter.
+    {{/regionalized}}
+        ///     - partition: AWS partition where service resides, standard (.aws), china (.awscn), government (.awsusgov).
+        ///     - endpoint: Custom endpoint URL to use instead of standard AWS servers
+        ///     - middleware: Middleware chain used to edit requests before they sent and responses before they are decoded 
+        ///     - timeout: Timeout value for HTTP requests
+        ///     - byteBufferAllocator: Allocator for ByteBuffers
+        ///     - options: Service options
+        {{scope}} init(
+            client: AWSClient,
+    {{#regionalized}}
+            region: SotoCore.Region? = nil,
+    {{/regionalized}}
+            partition: AWSPartition = .aws,
+            endpoint: String? = nil,
+            middleware: some AWSMiddlewareProtocol,
+            timeout: TimeAmount? = nil,
+            byteBufferAllocator: ByteBufferAllocator = ByteBufferAllocator(),
+            options: AWSServiceConfig.Options = []
+        ) {
+            self.client = client
+            self.config = AWSServiceConfig(
+    {{#regionalized}}
+                region: region,
+                partition: region?.partition ?? partition,
+    {{/regionalized}}
+    {{^regionalized}}
+                region: nil,
+                partition: partition,
+    {{/regionalized}}
+    {{#amzTarget}}
+                amzTarget: "{{.}}",
+    {{/amzTarget}}
+                service: "{{endpointPrefix}}",
+    {{#signingName}}
+                signingName: "{{.}}",
+    {{/signingName}}
+                serviceProtocol: {{protocol}},
+                apiVersion: "{{apiVersion}}",
+                endpoint: endpoint,
+    {{#first(serviceEndpoints)}}
+                serviceEndpoints: Self.serviceEndpoints,
+    {{/first(serviceEndpoints)}}
+    {{#first(partitionEndpoints)}}
+                partitionEndpoints: Self.partitionEndpoints,
+    {{/first(partitionEndpoints)}}
+    {{#first(variantEndpoints)}}
+                variantEndpoints: Self.variantEndpoints,
+    {{/first(variantEndpoints)}}
+    {{#errorTypes}}
+                errorType: {{.}}.self,
+    {{/errorTypes}}
+    {{#xmlNamespace}}
+                xmlNamespace: "{{.}}",
+    {{/xmlNamespace}}
+                middleware: AWSMiddlewareStack {
+                    middleware
+                    {{middlewareClass}}
+                },
+                timeout: timeout,
+                byteBufferAllocator: byteBufferAllocator,
+                options: options
+            )
+            {{#endpointDiscovery}}
+            self.endpointStorage = .init()
+            {{/endpointDiscovery}}
+        }
+    {{/middlewareClass}}
+
+    {{#first(serviceEndpoints)}}
+        /// custom endpoints for regions
+        static var serviceEndpoints: [String: String] {[
+    {{#serviceEndpoints}}
+            {{.}}{{^last()}},{{/last()}}
+    {{/serviceEndpoints}}
+        ]}
+    {{/first(serviceEndpoints)}}
+
+    {{#first(partitionEndpoints)}}
+        /// Default endpoint and region to use for each partition
+        static var partitionEndpoints: [AWSPartition: (endpoint: String, region: Region)] {[
+    {{#partitionEndpoints}}
+            {{.}}{{^last()}},{{/last()}}
+    {{/partitionEndpoints}}
+        ]}
+    {{/first(partitionEndpoints)}}
+
+    {{#first(variantEndpoints)}}
+        /// FIPS and dualstack endpoints
+        static var variantEndpoints: [EndpointVariantType: AWSServiceConfig.EndpointVariant] {[
+    {{#variantEndpoints}}
+            [{{variant}}]: .init(endpoints: [
+    {{#endpoints.endpoints}}
+                "{{region}}": "{{hostname}}"{{^last()}},{{/last()}}
+    {{/endpoints.endpoints}}
+            ]){{^last()}},{{/last()}}
+    {{/variantEndpoints}}
+        ]}
+    {{/first(variantEndpoints)}}
 
         // MARK: API Calls
     {{#operations}}
@@ -166,7 +263,7 @@ extension Templates {
     }
 
     extension {{ name }} {
-        /// Initializer required by `AWSService.with(middlewares:timeout:byteBufferAllocator:options)`. You are not able to use this initializer directly as there are no {{scope}}
+        /// Initializer required by `AWSService.with(middlewares:timeout:byteBufferAllocator:options)`. You are not able to use this initializer directly as there are no public
         /// initializers for `AWSServiceConfig.Patch`. Please use `AWSService.with(middlewares:timeout:byteBufferAllocator:options)` instead.
         {{scope}} init(from: {{ name }}, patch: AWSServiceConfig.Patch) {
             self.client = from.client
